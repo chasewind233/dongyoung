28706a38543543e056817cd3432bf0c31a6ed7a7
chasewind <1131158275@qq.com>
2019-05-23 18:50:58 +0800

    u

--- htmlwmj/renxi/love.js
+++ htmlwmj/renxi/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("杨丽丽", 23, 10);
+            ctx.fillText("Clock me! ", 23, 10);
             ctx.restore();
         },
         clear: function() {
